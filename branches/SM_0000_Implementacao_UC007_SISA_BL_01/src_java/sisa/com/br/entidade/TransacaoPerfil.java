6e260865becdf17813084a3e0072da8b6ddc5eb8
ricardovasc <ricardovasc@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-02-16 03:07:43 +0000

    Criando branch para desenvolvimento do caso de uso Manter Lote.

---
+++ branches/SM_0000_Implementacao_UC007_SISA_BL_01/src_java/sisa/com/br/entidade/TransacaoPerfil.java
@@ -0,0 +1,28 @@
+package sisa.com.br.entidade;
+
+
+public class TransacaoPerfil {
+ 
+	private int IdPerfil;
+	
+	private int IdTransacao;
+	
+	
+	public int getIdPerfil() {
+		return IdPerfil;
+	}
+
+	public void setIdPerfil(int IdPerfil) {
+		this.IdPerfil = IdPerfil;
+	}
+
+	public int getIdTransacao() {
+		return IdTransacao;
+	}
+
+	public void setIdTransacao(int IdTransacao) {
+		this.IdTransacao = IdTransacao;
+	}
+	
+}
+ 
