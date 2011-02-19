acda9d97d3815671b1fe17486064b71a3b429db4
rui.sampaio.jr <rui.sampaio.jr@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-02-19 21:56:01 +0000

    Caso de Uso UC005

---
+++ sisa/Implementação/workspace/JavaFlex/src_java/sisa/com/br/repositorio/hibernate/PromitenteHibernateDao.java
@@ -0,0 +1,66 @@
+package sisa.com.br.repositorio.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import sisa.com.br.entidade.Promitente;
+import sisa.com.br.repositorio.PromitenteRepository;
+
+@Repository(value="promitenteRepository")
+public class PromitenteHibernateDao extends HibernateDaoSupport implements PromitenteRepository {
+
+	@Autowired
+	public PromitenteHibernateDao(@Qualifier("sessionFactory")SessionFactory factory) {
+		super.setSessionFactory(factory);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Promitente> consultaPromitente(Promitente promitente)
+			throws Exception {
+		Criteria criteria = getSession().createCriteria(Promitente.class,
+		"promitente");
+		if (promitente != null) {
+		
+			if (promitente.getCpfCnpj() != null) {
+				criteria.add(Restrictions.eq("promitente.cpfCnpj", promitente.getCpfCnpj()));
+			}
+			if (promitente.getNome() != null) {
+				criteria.add(Restrictions.like("promitente.nome", promitente
+						.getNome(), MatchMode.START));
+			}
+		}
+		//	        
+		return criteria.list();
+	}
+
+	public Promitente findById(String id) throws Exception {
+		Promitente promitente = (Promitente) getHibernateTemplate().get(Promitente.class, id);
+		
+		if (promitente == null)
+			throw new Exception("O Promitente com a código: "+id+" do(a) "+Promitente.class.getSimpleName()+" não foi encontrado.");
+		return promitente;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Promitente> getList() throws Exception {
+		return (List<Promitente>) getHibernateTemplate().loadAll(Promitente.class);
+	}
+
+	public void remove(Promitente promitente) throws Exception {
+		getHibernateTemplate().delete(promitente);
+	}
+
+	public Promitente save(Promitente promitente) throws Exception {
+		getHibernateTemplate().saveOrUpdate(promitente);
+		return promitente;
+	}
+
+}
