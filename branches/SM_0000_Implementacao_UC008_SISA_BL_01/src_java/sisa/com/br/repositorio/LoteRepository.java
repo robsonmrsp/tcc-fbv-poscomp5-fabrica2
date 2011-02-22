c1effbf9206265202dec1a4ad3564d6f0fd6f171
leofelipe.a <leofelipe.a@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-02-22 22:59:12 +0000

    Subindo a parte de Loteamento e a parte inicial de Lote para serem utilizados pelos demais

---
+++ branches/SM_0000_Implementacao_UC008_SISA_BL_01/src_java/sisa/com/br/repositorio/LoteRepository.java
@@ -0,0 +1,15 @@
+package sisa.com.br.repositorio;
+
+import java.util.List;
+
+import sisa.com.br.entidade.Lote;
+
+
+public interface LoteRepository {
+	Lote consultaLote(String nu_lote,String nu_lotm,String nu_quadra) throws Exception;
+	Lote save(Lote lote) throws Exception;
+	Lote findById(Lote lote) throws Exception;
+	List<Lote> getList() throws Exception;
+	List<Lote> consultaLote(Lote lote)throws Exception;
+	void remove(Lote lote) throws Exception;
+}
