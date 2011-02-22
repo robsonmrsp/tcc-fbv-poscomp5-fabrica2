aae9440159b0200eab117491d78babe2eea05757
ricardovasc <ricardovasc@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-02-22 00:43:01 +0000

    Versão para desenvolvedores.

--- sisa/Análise e Design/Modelo de Dados/scripts/SISA_ANA_Carga_Endereco.sql
+++
@@ -1,5 +0,0 @@
-﻿
-INSERT INTO endereco(cd_endereco, nu_cep, ds_logradouro, nr_endereco, ds_complemento, ds_bairro, cd_uf, no_cidade)
-VALUES (1, '54500000', 'Estrada do Bongi', 1155, 'AP-210', 'Prado', 'PE', 'RECIFE');
-
-
--- sisa/Análise e Design/Modelo de Dados/scripts/SISA_ANA_Carga_Promitente.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO promitente(cd_pro, nome_promitente, dominio_estado_civil, dominio_sexo, nu_rg, ds_org_emiss, dt_exp_rg, nu_cgc_cpf, dt_nasc, fl_dep, nu_fone_res, cd_end_cobr, nu_fone_cobr,
-ds_profissao, situacao, nacionalidade, cd_end_residencial, nome_responsavel, nu_cgc_cpf_responsavel)
-VALUES (1, 'SILVIO ALMEIDA DA SILVA', 'S', 'M', '5560670', 'SSP-PE', '1995-05-01', '031.583.084-01', '1978-10-02', 'N', '34313256', 1, '34322225', 'AUTONOMO', 'A', 'BRASILEIRO', 1, NULL, NULL);
-
-INSERT INTO promitente(cd_pro, nome_promitente, dominio_estado_civil, dominio_sexo, nu_rg, ds_org_emiss, dt_exp_rg, nu_cgc_cpf, dt_nasc, fl_dep, nu_fone_res, cd_end_cobr, nu_fone_cobr,
-ds_profissao, situacao, nacionalidade, cd_end_residencial, nome_responsavel, nu_cgc_cpf_responsavel)
-VALUES (2, 'LEONARDO RODRIGO', 'S', 'M', '5560679', 'SSP-PE', '1998-05-03', '031.583.084-01', '1980-10-02', 'N', '34313266', 1, '34322225', 'AUTONOMO', 'A', 'BRASILEIRO', 1, NULL, NULL);
