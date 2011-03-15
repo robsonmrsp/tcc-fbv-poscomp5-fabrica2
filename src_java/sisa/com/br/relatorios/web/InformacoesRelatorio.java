55cc0469f2dbfce0098b818b8967788753871b8e
mcvpss <mcvpss@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-03-15 23:52:24 +0000

    versão que gerou o relatório com sucesso!

--- src_java/sisa/com/br/relatorios/web/InformacoesRelatorio.java
+++ src_java/sisa/com/br/relatorios/web/InformacoesRelatorio.java
@@ -1,35 +1,24 @@
 package sisa.com.br.relatorios.web;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import sisa.com.br.relatorios.ConexaoMySQL;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.view.JasperViewer;
-import sisa.com.br.relatorios.ConexaoMySQL;
 
 /**
  * Classe responsÃ¡vel por armazenar as informaÃ§Ãµes referente ao relatÃ³rio.
  * 
  */
-public class InformacoesRelatorio extends HttpServlet{
+public class InformacoesRelatorio {
 	
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	private String identicacaoRelatorio;	
 
@@ -39,8 +28,8 @@ public class InformacoesRelatorio extends HttpServlet{
 	
 	static String complDiretorioJaspers = "relatorios/";
 	static String complDiretorioImagens = "relatorios/imagens/";
-	static String nomeImgCidadeGarapu = complDiretorioImagens + "cidade garapu.jpg";
-	static String nomeImgLogoSisa = complDiretorioImagens + "logo sisa.jpg";
+	static String nomeImgCidadeGarapu = complDiretorioImagens + "cidade_garapu.jpg";
+	static String nomeImgLogoSisa = complDiretorioImagens + "logo_sisa.jpg";
 	static String caminhoProjeto = System.getProperty("user.dir");
 	
 	// Construtores
@@ -86,7 +75,6 @@ public class InformacoesRelatorio extends HttpServlet{
 	}
 	
 	public void gerar() throws JRException, SQLException {
-		System.out.println("inicio de geração de relatório!");
     	// Cria a conexão
         Connection conexao = new ConexaoMySQL().getConnection();
         conexao.createStatement();
@@ -96,75 +84,23 @@ public class InformacoesRelatorio extends HttpServlet{
 //        arquivoAux = new File(arquivoAux.getClass().getResource("/").getPath() + complDiretorioJaspers + identicacaoRelatorio);
 //        System.out.println(arquivoAux.exists());
         
-        JasperPrint jp = 
-        	JasperFillManager.fillReport(arquivoAux.getClass().getResource("/").getPath() + "/" + complDiretorioJaspers + identicacaoRelatorio , 
-        			getMapaParametros(), conexao);            
+//        JasperPrint jp = JasperFillManager.fillReport(arquivoAux.getClass().getResource("/").getPath() + "/" + complDiretorioJaspers + identicacaoRelatorio , getMapaParametros(), conexao);            
+        JasperPrint jp = JasperFillManager.fillReport(identicacaoRelatorio , getMapaParametros(), conexao);            
         
         // Exporta para PDF
-//        JasperExportManager.exportReportToPdfFile(jp, nomeArquivoSaida);
+        JasperExportManager.exportReportToPdfFile(jp, nomeArquivoSaida);
         
-        JasperViewer jrv = new JasperViewer(jp, false);
+        System.out.println("Arquivo Gerado com Sucesso em: " + nomeArquivoSaida);
+        
+        JasperViewer jrv = new JasperViewer(jp);
         jrv.setVisible(true);
         
     	String arquivoSaida = caminhoProjeto + "/" + nomeArquivoSaida;
         
         System.out.println("Relatório Gerado com Sucesso em: " + arquivoSaida);
         
-        conexao.close();
+        //conexao.close();
 		
-	}
-	
-	public String gerarXML() throws JRException, SQLException {
-		String xmlRetorno = "";
-		System.out.println("inicio de geração de relatório XML!");
-    	// Cria a conexão
-        Connection conexao = new ConexaoMySQL().getConnection();
-        conexao.createStatement();
-        
-        File arquivoAux = new File(identicacaoRelatorio);
-        
-//        arquivoAux = new File(arquivoAux.getClass().getResource("/").getPath() + complDiretorioJaspers + identicacaoRelatorio);
-//        System.out.println(arquivoAux.exists());
-        
-        JasperPrint jp = 
-        	JasperFillManager.fillReport(arquivoAux.getClass().getResource("/").getPath() + "/" + complDiretorioJaspers + identicacaoRelatorio , 
-        			getMapaParametros(), conexao);            
-        jp.setName(nomeArquivoSaida);
-        xmlRetorno = JasperExportManager.exportReportToXml(jp);
-        
-    	String arquivoSaida = caminhoProjeto + "/" + nomeArquivoSaida;
-        
-        System.out.println("Relatório Gerado com Sucesso em: " + arquivoSaida);
-        
-        conexao.close();
-		
-        return xmlRetorno;
-	}
-	
-	protected void processarRequisicao(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		String reportXML = "";
-		try {
-			reportXML = gerarXML();
-		} catch (JRException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		response.setContentType("text/xml"); //Configura o ContentType
-		response.setCharacterEncoding("ISO 8859 1");//- - Configura o Character Enconding
-		PrintWriter outputStream = response.getWriter();
-		outputStream.print(reportXML); // Imprime o xml
-	}
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
-		processarRequisicao(request, response);
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
-		processarRequisicao(request, response);
-	}
-	public String getServletInfo() {
-		return "Short description";
 	}
 
 }
