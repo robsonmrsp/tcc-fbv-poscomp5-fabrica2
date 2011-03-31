efa3f38a5b20f75fd701010a602fbe5d2e1dc325
ricardovasc <ricardovasc@c459739d-1b2d-c82c-0ecf-20d8b2153ebf>
2011-03-31 02:05:53 +0000

    Merge da Baseline SISA_BL_03 com o trunk.

--- sisa/Implementação/workspace/JavaFlex/src_java/sisa/com/br/repositorio/PromitenteRepository.java
+++ sisa/Implementação/workspace/JavaFlex/src_java/sisa/com/br/repositorio/PromitenteRepository.java
@@ -9,7 +9,7 @@ public interface PromitenteRepository {
 	Promitente save(Promitente promitente) throws Exception;
 	Promitente findById(String id) throws Exception;
 	List<Promitente> getList() throws Exception;
-	List<Promitente> consultaPromitente(Promitente promitente)throws Exception;
+	List<Promitente> find(Promitente promitente)throws Exception;
 	void remove(Promitente promitente) throws Exception;
 
 }
--- sisa/Implementação/workspace/JavaFlex/src_java/sisa/com/br/service/DominioService.java
+++ sisa/Implementação/workspace/JavaFlex/src_java/sisa/com/br/service/DominioService.java
@@ -2,8 +2,13 @@ package sisa.com.br.service;
 
 import java.util.List;
 
+import sisa.com.br.dominio.objDominio;
+
 public interface DominioService {
 
-	List<String> listarTipoSexo() throws Exception;
-	
+	List<objDominio> listarTipoSexo() throws Exception;
+	List<objDominio> listarEstadoCivil() throws Exception;
+	List<objDominio> listarSituacoesPropostas() throws Exception;
+	List<objDominio> listarTipoPagto() throws Exception;
+	List<objDominio> listarSituacaoProposta() throws Exception;
 }
