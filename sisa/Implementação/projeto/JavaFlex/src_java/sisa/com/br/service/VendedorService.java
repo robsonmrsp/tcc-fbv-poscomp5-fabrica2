6a6efd119e90e6f56b15f80c11c3fda042dcc941
viniciusaraiva <viniciusaraiva@gmail.com>
2011-04-10 23:24:39 +0000

    correção erros

--- sisa/Implementação/projeto/JavaFlex/src_java/sisa/com/br/service/VendedorService.java
+++ sisa/Implementação/projeto/JavaFlex/src_java/sisa/com/br/service/VendedorService.java
@@ -6,13 +6,11 @@ import sisa.com.br.entidade.Vendedor;
 
 public interface VendedorService {
 
-	Vendedor consultaVendedor(String vendedor) throws Exception;
+	//Vendedor consultaVendedor(String vendedor) throws Exception;
 	
 	Vendedor save(Vendedor vendedor) throws Exception;
 	Vendedor findById(Vendedor vendedor) throws Exception;
 	List<Vendedor> getList() throws Exception;
 	List<Vendedor> consultaVendedor(Vendedor vendedor)throws Exception;
 	void remove(Vendedor vendedor) throws Exception;
-	
-	public void pingSessao();
 }
